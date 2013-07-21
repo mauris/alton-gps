051ae901b52bbeb8b108584d6a20a14301e3b9b4
Sam-Mauris Yong <mauris@hotmail.sg>
2013-07-21 17:11:34 +0800

    updated index

--- web/index.php
+++ web/index.php
@@ -1,9 +1,5 @@
 <?php
-namespace Packfire\Core;
 
-use Packfire\Packfire;
-use Packfire\Core\ClassLoader\ClassFinder;
-use Packfire\Core\ClassLoader\ClassLoader;
 use Packfire\Application\Http\Application as HttpApplication;
 
 /**
@@ -17,29 +13,7 @@ use Packfire\Application\Http\Application as HttpApplication;
  * @ignore
  */
 
-require('pack/constants.php');
-
-$path = null;
-if(__PACKFIRE_ROOT__){
-    $path = __PACKFIRE_ROOT__;
-}else{
-    $namespaces = require('vendor/composer/autoload_namespaces.php');
-    if($namespaces){
-        $path = $namespaces['Packfire'];
-    }
-}
-
-if($path){
-    define('__PACKFIRE_START__', microtime(true));
-    // include the main Packfire class
-    require $path . '/Packfire/Packfire.php';
-    $packfire = new Packfire();
-    $packfire->classLoader()->register(true);
-    $finder = new ClassFinder();
-    $finder->addPath('pack/src/');
-    $loader = new ClassLoader($finder);
-    $loader->register(true);
+$packfire = require(__DIR__ . '/../app/bootstrap.php');
+if($packfire){
     $packfire->fire(new HttpApplication());
-}else{
-    throw new \Exception('Could not bootstrap test because Packfire Framework was not installed.');
 }
